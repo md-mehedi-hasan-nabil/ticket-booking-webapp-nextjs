a5a1df21eb5abb4877b1c0e775a3a2395f8e1ae2
Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2021-10-31 19:21:20 +0600

    first commi changet

--- pages/_document.js
+++ pages/_document.js
@@ -10,13 +10,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,300&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+          <style>
+            @import
+            url(`https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,300&display=swap`);
+          </style>
         </Head>
         <body>
           <Main />
